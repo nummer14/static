4b2658c911c36e5f6ffeff03f1fa152bdc56e7a3
numme <nummer14@naver.com>
2025-06-16 10:29:26 +0900

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 class Person {
+    int a= 0;
+    static int b = 1;
+
     public static void greet() {
         System.out.println("Hello from static method!");
+        // System.out.println(a); 접근 불가
+        System.out.println(b);
     }
 
     public void sayHello() {
