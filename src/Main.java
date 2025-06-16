061e1d8f287a69ccb710fdad4ae3e11b3c0e37c7
numme <nummer14@naver.com>
2025-06-16 11:41:03 +0900

    Fifth Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    String outerMessage = "Hello from Outer";
+
+
+    class Inner {
+        void showMessage() {
+            System.out.println(outerMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        Main.Inner inner = main.new Inner();
+
+        inner.showMessage();
+    }
+}
